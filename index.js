99f3b68b64c859868a9557eafebe71e200401dcc
bhashlamoon <bara1brh@gmail.com>
2026-04-07 20:26:11 +0300

    Render posts dynamically and add like toggle
    Add post UI styles and dynamic rendering with like functionality. A new filled-heart asset was added. index.css: tweak header/ avatar spacing and add layout/styles for .post, .post-author, actions, likes, and captions. index.html: move script to end and replace static post markup with <main id="main-content"> placeholder. index.js: add isLiked to posts, implement renderPosts() to build posts from data, add dblclick handler to like images, and toggleLike() to increment/decrement likes and re-render.

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const posts = [
         avatar: "images/avatar-vangogh.jpg",
         post: "images/post-vangogh.jpg",
         comment: "just took a few mushrooms lol",
-        likes: 21
+        likes: 21,
+        isLiked: false,
     },
     {
         name: "Gustave Courbet",
@@ -15,7 +16,8 @@ const posts = [
         avatar: "images/avatar-courbet.jpg",
         post: "images/post-courbet.jpg",
         comment: "i'm feelin a bit stressed tbh",
-        likes: 4
+        likes: 4,
+        isLiked: false,
     },
         {
         name: "Joseph Ducreux",
@@ -24,7 +26,57 @@ const posts = [
         avatar: "images/avatar-ducreux.jpg",
         post: "images/post-ducreux.jpg",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
+        likes: 152,
+        isLiked: false,
     }
 ]
+
+const mainContentEl=document.querySelector("#main-content");
+mainContentEl.addEventListener("dblclick", function(e){
+    const postImg = e.target.closest(".post-image");
+    if (!postImg) return;
+
+    const index = postImg.dataset.index;
+    toggleLike(index);
+});
+
+function renderPosts(){
+    let postsHtml="";
+    for (let i in posts){
+        const post = posts[i];
+        postsHtml+=`
+        <div class="post">
+                <div class="post-author">
+                    <img class="avatar" src="${post.avatar}" alt="post author avatar">
+                    <div class="author-info">
+                        <h3 class="author-name">${post.name}</h3>
+                        <h4 class="image-location">${post.location}</h4>
+                    </div>
+                </div>
+                <img class="post-image" data-index="${i}" src="${post.post}" alt="a post image of painting by ${post.name}">
+                <div class="post-action" >
+                    <button class="action-btn" onclick="toggleLike(${i})"><img class="btn-like" src="${post.isLiked ? 'images/icon-heart-fill.png':'images/icon-heart.png'}" alt=""></button>
+                    <button class="action-btn"><img class="btn-comment" src="images/icon-comment.png" alt=""></button>
+                    <button class="action-btn"><img class="btn-dm" src="images/icon-dm.png" alt=""></button>
+                </div>
+                <h3 class="likes-counter">${post.likes} likes</h3>
+                <p class="post-caption"><span class="author-username">${post.username}</span> ${post.comment}</p>   
+            </div>
+        `;
+    }
+    mainContentEl.innerHTML=postsHtml;
+}
+renderPosts();
+
+function toggleLike(index){
+    const post = posts[index];
+    if (post.isLiked){
+        post.likes--;
+    }
+    else{
+        post.likes++;
+    }
+    post.isLiked=!post.isLiked;
+    renderPosts();  
+}
 
